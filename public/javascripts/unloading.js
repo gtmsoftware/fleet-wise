ac0070be82b6ef4b093d66ef00d489ffa91c5482
Ankur Gupta <ankurdotcom@gmail.com>
2023-11-01 19:31:30 +0530

    validation added in forms

--- public/javascripts/unloading.js
+++ public/javascripts/unloading.js
@@ -1,8 +1,8 @@
-// window.addEventListener('load', () => {
-//     var now = new Date();
-//     now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
-//     now.setMilliseconds(null)
-//     now.setSeconds(null)
+window.addEventListener('load', () => {
+    var now = new Date();
+    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+    now.setMilliseconds(null)
+    now.setSeconds(null)
   
-//     document.getElementById('loadingDate').value = now.toISOString().slice(0, -1);
-//   });
+    document.getElementById('unloadingDate').value = now.toISOString().slice(0, -1);
+});
