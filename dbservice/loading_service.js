f10677ccfae9ecad0ee665b5999d33431043ae7d
Ankur Gupta <ankurdotcom@gmail.com>
2023-11-03 17:29:59 +0530

    loading form updated

--- dbservice/loading_service.js
+++ dbservice/loading_service.js
@@ -1,18 +1,10 @@
 const dbConnectionPool = require('../dbservice/db_pool');
 
 /**
- * Check DB Connection Pool Intilised
- */
-!(async () => {
-  const result = await dbConnectionPool.query('SELECT NOW()');
-  result ? console.log(' DB Connection Success ') : console.error(' DB Connection Failed ');
-})();
-
-
-/**
  * 
- * SELECT sale_point, buyer, cr_dr, vehiclenumber, rokar, loadingid, "timestamp", loaddt, weight, rate, total FROM fleetwise_schema.loading;
+ * @param { Roker Number } rokar 
  * 
+ * Will Return Loading Record Based on Rokar Number
  */
 const getLoading = async (rokar) => {
   console.log("Started :: Get Loading");
@@ -25,11 +17,25 @@ const getLoading = async (rokar) => {
   try {
     const result = await dbConnectionPool.query(query);
     console.log('Loading Entry Fetched :', result.rows);
+    //console.log('Loading Entry Fetched 2:', result.rows);
+    return result.rows;
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
+/**
+ * 
+ * @param { Loading Date } loaddt 
+ * @param { Sales Point } sale_point 
+ * @param { Buyer } buyer 
+ * @param { Weight } weight 
+ * @param { Rate } rate 
+ * @param { Total } total 
+ * @param { Credit Or Debit } cr_dr 
+ * @param { Vehicle Number } vehiclenumber 
+ * @param { ROKAR Number } rokar 
+ */
 const saveLoading = async (loaddt, sale_point, buyer, weight, rate, total, cr_dr, vehiclenumber, rokar) => {
   console.log("Started :: Save Loading");
   const query = {
--- routes/loading.js
+++ routes/loading.js
@@ -7,7 +7,29 @@ const pageName = 'Loading Entry Page';
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('loading', { title: `${pageName}`, message: `${pageName}` });
+  const rokar = '2023/NOV/028';
+  const record = {};
+
+  res.render('loading', { title: `${pageName}`, message: `${pageName}`, record: `${record}` });
+});
+
+
+/* GET users listing. */
+router.get('/:rokar', async function(req, res, next) {  
+  
+  console.log(`Getting Loading Entry Record Using Rokar : ${req.params.rokar}`);
+  //const rokar = '2023-NOV-028';
+  if(!req.params.rokar){
+    const errorMsg = 'Request with valid Rokar number';
+    console.error('Error:', errorMsg);
+    res.render('loading', { title: `${pageName}`, message: `${errorMsg}`, record: `{}` });
+  }
+
+  const recordList = await loading_service.getLoading(req.params.rokar);
+
+  console.log(recordList[0]);
+
+  res.render('loading', { title: `${pageName}`, message: `${pageName}`, record: `${recordList[0].vehiclenumber}` });
 });
 
 
@@ -17,12 +39,14 @@ router.post('/', function(req, res, next) {
     console.log(`req==>${JSON.stringify(req.body)}`);
     
     const vehicaleNum = 'UP77 N 1234';
-    const rokar = '2023/NOV/028';
+    const rokar = '2023-NOV-028';
 
     const { loadingDate, ponitSale, buyer, weight, rate, total, cr_dr } = req.body;
     
     loading_service.saveLoading(loadingDate, ponitSale, buyer, weight, rate, total, cr_dr, vehicaleNum, rokar);
 
-    res.render('loading', { title: `${pageName}`, message: 'Loading Entry Saved' });
+    res.render('loading', { title: `${pageName}`, message: 'Loading Entry Saved', record: '' });
   });
+
+
 module.exports = router;
