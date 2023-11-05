88f3703ede8247c1b514393ad86466ddbd93a574
Ankur Gupta <ankurdotcom@gmail.com>
2023-11-06 02:22:47 +0530

    removed sesitive information from code base

--- dbservice/common/db_pool.js
+++ dbservice/common/db_pool.js
@@ -4,19 +4,19 @@ const Pool = require('pg').Pool;
 /**
  * Start Database Connection Pool
  */
+console.log(process.env);
 
 const dbConnectionPool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'oum',
-  password: 'gtmSoftware',
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
+  max: process.env.DB_MAX,
+  idleTimeoutMillis: process.env.DB_IDLE_TIMEOUT_MS,
+  connectionTimeoutMillis: process.env.DB_IDLE_TIMEOUT_MS,
 });
 
 // Refrence : https://node-postgres.com/apis/pool
-
 module.exports = dbConnectionPool;
 
