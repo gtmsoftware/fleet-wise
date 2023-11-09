b63a3bfeecbba6307365c4ae60f935db1a86ae31
Ankur Gupta <ankurdotcom@gmail.com>
2023-11-10 00:54:40 +0530

    new rate limmiter middle ware used

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 const cookieSession = require('cookie-session');
+const rateLimit = require('express-rate-limit');
+
 
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
@@ -35,7 +37,19 @@ app.use(helmet());
 // Added Rate Limitter to prevent Bruit-force Attack
 const rateLimiterRedisMiddleware = require('./middleware/rateLimiterRedis');
 // Uncomment it for Production
-app.use(rateLimiterRedisMiddleware);
+// app.use(rateLimiterRedisMiddleware);
+
+//
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 
 // view engine setup
